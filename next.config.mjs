7ac36e8830728e6f2a569d9aba297cf5f38986c8
Diyaeeddine <58663449+Diyaeeddine@users.noreply.github.com>
2025-04-02 17:35:33 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    
+  experimental: {
+    serverActions: true, // Required for Clerk v4+
+  },
     images: {
         remotePatterns: [
             {
