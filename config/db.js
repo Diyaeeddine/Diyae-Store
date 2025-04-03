4971a124949eb43de40455830e2d68bff97f1073
diyaeeddine <diyaetube0@gmail.com>
2025-04-03 15:13:41 +0000

    .env and db.jd updates

--- config/db.js
+++ config/db.js
@@ -1,24 +1,32 @@
 import mongoose from "mongoose";
+
 let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
+
 async function connectDB() {
   if (cached.conn) {
     return cached.conn;
   }
+
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
     };
+
     cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/nextjs-cluster`, opts)
-      .then((mongoose) => {
-        return mongoose;
+      .connect(process.env.MONGODB_URI, opts) // Removed the `/nextjs-cluster` part
+      .then((mongoose) => mongoose)
+      .catch((error) => {
+        console.error("MongoDB Connection Error:", error);
+        throw error;
       });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
+
 export default connectDB;
