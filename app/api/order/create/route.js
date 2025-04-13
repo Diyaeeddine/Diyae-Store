9ca410f075772df084bee0a7255baca971673276
diyaeeddine <diyaetube0@gmail.com>
2025-04-13 23:26:28 +0100

    finished

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -8,29 +8,34 @@ export async function POST(request) {
   try {
     const { userId } = getAuth(request);
     const { address, items } = await request.json();
-    if (!address || items.length == 0) {
+
+    if (!address || items.length === 0) {
       return NextResponse.json({ success: false, message: "Invalid data" });
     }
-    //calculate amount using items
+
     const amount = await items.reduce(async (acc, item) => {
-      const product = await Product.findById(item, product);
+      acc = await acc;
+      const product = await Product.findById(item.product);
       return acc + product.offerPrice * item.quantity;
-    }, 0);
+    }, Promise.resolve(0));
+
     await inngest.send({
       name: "order/created",
       data: {
         userId,
         address,
         items,
-        amount: amount + Math.floor(amount * 0.02),
+        amount: amount + Math.floor(amount * 0.02), 
         date: Date.now(),
       },
     });
-    //Clear user cart
+
+    // Clear user cart
     const user = await User.findById(userId);
     user.cartItems = {};
     await user.save();
-    return NextResponse.json({ success: true, message: "order placed" });
+
+    return NextResponse.json({ success: true, message: "Order placed" });
   } catch (error) {
     return NextResponse.json({ success: false, message: error.message });
   }
